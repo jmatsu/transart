871a001275614003db9eaaf659d6379a42af9311
Jumpei Matsuda <jmatsu.drm@gmail.com>
2018-11-25 01:15:14 +0900

    Fixed a test for Asset

--- github/entity/asset_test.go
+++ github/entity/asset_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestAsset(t *testing.T) {
-	bytes := []byte("{\"id\": 10, \"state\": \"this is a state\", \"name\": \"this is a name\"}")
+	bytes := []byte("{\"id\": 10, \"state\": \"this is a state\", \"name\": \"this is a name\", \"size\": 1}")
 
 	asset := Asset{}
 
